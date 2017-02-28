697f8d8476d990b7ec23aa543f4d490f80e1dc1b
Mayank Kumar <mayankkumariiitm@gmail.com>
2017-02-28 13:53:40 +0530

    Container base complete

--- src/Yaf/Container/Container.php
+++ src/Yaf/Container/Container.php
@@ -1,59 +1,69 @@
 <?php
-
 namespace Yaf\Container;
-use Yaf\Contract\Container\Container as ContainerContract;
-use Closure;
+
 use ArrayAccess;
+use Closure;
+use Yaf\Contract\Container\Container as ContainerContract;
 use ReflectionClass;
+use LogicException;
+use ReflectionParameter;
+use ReflectionException;
 
 class Container implements ContainerContract, ArrayAccess
 {
-    protected $services;
+    public $services=[];
     
-    protected $instances;
+    protected $instances=[];
     
-    public function bind ($service, $class=null)
+    protected $buildStack=[];
+
+    public function bind($service, $class=null) 
     {
         if(is_null($class)){
             $class=$service;
         }
-        $this->services[$service]=$this->getClosure($class);
-        return $this;
+        $this->services[$service]= $this->getClosure($class);
     }
     
     protected function getClosure($class)
     {
-        if($class instanceof Closure){
-            return $class();
+        if ($class instanceof Closure) {
+            return $class;
         }
         
-        return function () use ($class){
-            return $this->resolve($class);
+        return function() use ($class){
+          return $this->resolve($class);  
         };
     }
+    
     public function make($service)
     {
-        if(isset($this->services[$service])){
-            $class= $this->services[$service];
-        }else{
-            $class=$service;
-            $this->bind($service);
-        }
+        $class=$this->services[$service] ?? $service;
         return $this->resolve($class);
     }
-    public function resolve($class)
+    
+    public function getBinding($service)        
+    {
+        return $this->services[$service] ?? null;
+    }
+    
+    protected function resolve($class)
     {
         if ($class instanceof Closure) {
             return $class();
         }
-        $reflector=new ReflectionClass($class);
-        if(!$reflector->isInstantiable()){
-            throw new Exception("class $class in not instantiable");
+        try {
+            $reflection = new ReflectionClass($class);   
+        } catch (ReflectionException $e) {
+            return $this->notInstantiable($class);
         }
-        
-        $constructor = $reflector->getConstructor();
-        
+        if(!$reflection->isInstantiable()){
+            return $this->notInstantiable($class);
+        }
+        $this->buildStack[]=$class;
+        $constructor=$reflection->getConstructor();
         if(is_null($constructor)){
+            array_pop($this->buildStack);
             return new $class;
         }
         
@@ -61,39 +71,56 @@ class Container implements ContainerContract, ArrayAccess
         $dependencies=[];
         
         foreach ($parameters as $parameter) {
-            if($parameter->isOptional()){
-                $dependencies[]=$parameter->getDefaultValue();
-            }else{
-                $dependencies[]=$this->resolve($parameter->getClass()->name);
-                
-            }
+            $dependencies[]=$this->resolveDependencies($parameter);
         }
-        return $reflector->newInstanceArgs($dependencies);
+        return $reflection->newInstanceArgs($dependencies);
     }
-
-    public function offsetExists($offset): bool
+    
+    protected function resolveDependencies(ReflectionParameter $parameter) 
     {
-        return isset($this->services[$offset]);
-    }
-
-    public function offsetGet($offset)
-    {
-        if($this->offsetExists($offset)){
-            return $this->make($offset);
+        if($parameter->isOptional()){
+            return $parameter->getDefaultValue();
         }
-        return null;
+        return $this->make($parameter->getClass()->name);  
     }
-
-    public function offsetSet($offset, $value): void
+    protected function notInstantiable($class)
     {
-        $this->bind($offset, $value);
-    }
+        if (! empty($this->buildStack)) {
+            $previous = implode(', ', $this->buildStack);
 
-    public function offsetUnset($offset): void
-    {
-        if($this->offsetExists($offset)){
-            unset($this->services[$offset]);
+            $message = "Target [$class] is not instantiable while building [$previous].";
+        } else {
+            $message = "Target [$class] is not instantiable.";
         }
+        throw new LogicException($message);
+    }
+    
+    public function instance($service, $instance) {
+        
+    }
+
+    public function rebind($service, $class) {
+        
+    }
+
+    public function unbind($service) {
+        
+    }
+
+    public function offsetExists($offset): bool {
+        
+    }
+
+    public function offsetGet($offset) {
+        
+    }
+
+    public function offsetSet($offset, $value): void {
+        
+    }
+
+    public function offsetUnset($offset): void {
+        
     }
 
 }
--- src/Yaf/Contract/Container/Container.php
+++ src/Yaf/Contract/Container/Container.php
@@ -2,4 +2,9 @@
 namespace Yaf\Contract\Container;
 interface Container {
     public function bind($service,$class);
+    public function unbind($service);
+    public function rebind($service,$class);
+    public function getBinding($service);
+    public function make($service);
+    public function instance($service,$instance);
 }
